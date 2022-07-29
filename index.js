84a95b0da290f4fe3643e9173cc01d0c0202df96
Ron Dronet <ron@dronetdevdesign.com>
2022-07-29 10:52:11 -0500

    creating styles for index.html generated file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { writeFile } = require('node:fs/promises');
-const generateMarkdown = require("./src/generateMarkdown");
+const generateMarkdown = require('./src/generateMarkdown');
 
 // array of questions when prompted
 const questions = [
   {
     type: 'input',
-    name: 'title', 
-    message: 'What is your name?', 
+    name: 'name',
+    message: 'What is your name?',
     validate: (titleInput) => {
-      if(titleInput) {
+      if (titleInput) {
         return true;
       } else {
         console.log('Please enter your name!');
@@ -34,16 +34,16 @@ function writeToFile(fileName, data) {
 // function to initialize app
 function init() {
   inquirer.prompt(questions)
-  .then((answers) => {
-  writeToFile('./dist/index.html', answers);
-  }) 
-  .catch((error) => {
-  if (error.isTypeError) {
-    throw new Error("TypeError" + error.message);
-    } else {
-    throw new Error(error);
-    }
-  });
+    .then((answers) => {
+      writeToFile('./dist/index.html', answers);
+    })
+    .catch((error) => {
+      if (error.isTypeError) {
+        throw new Error('TypeError' + error.message);
+      } else {
+        throw new Error(error);
+      }
+    });
 }
 
 // initialize app
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,22 +1,58 @@
+// function used to toggle github or school choice selected by manager.
+choiceData = (choice) => {
+  let selectedChoice = '';
+  if ('Intern') {
+    return selectedChoice.data;
+  } else if ('Engineer') {
+    return selectedChoice.data;
+  } else {
+    selectedChoice = `Office number: ${data}`;
+  }
+  console.log(choice);
+}
+
 // function that generates the html markdown
 generateMarkdown = (data) => {
- const { name, id, email, role } = data;
- return `
- <!DOCTYPE html>
+  const { name, id, email, role } = data;
+  return `
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Team Dash</title>
-  <link rel="stylesheet" href="./dist/style.css">
+  <link rel="stylesheet" href="../dist/style.css">
 </head>
 <body>
-  
+
+  <header class="sticky-header">
+    <h1 class="header-title">TEAM<span class="dash">&#xbb;</span><span class="dash-word">DASH</span></h1>
+    <h3 class="subhead">Employee Information Dashboard</h3>
+  </header>
+
+  <div class="card-container">
+    <div class="employee-card">
+      <div class="card-header">
+        <div class="card-header-content">
+          <h1 class="employee-name">${data.name}</h1>
+          <h3 class="employee-role">${data.role}</h3>
+        </div>
+      </div>
+      <div class="employee-data">
+        <ul class="data-list">
+          <li class="list-item">ID:${data.id}</li>
+          <li class="list-item">Email: <a href = "mailto: ${data.email}">${data.email}</a></li>
+          <li class="list-item">${choiceData.data}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
- 
  `;
+  generateMarkdown(data);
 }
 
 module.exports = generateMarkdown;
