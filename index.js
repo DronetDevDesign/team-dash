b6e9981d4b721a75e853236c76f9bdf87459f345
Ron Dronet <ron@dronetdevdesign.com>
2022-07-30 10:22:53 -0500

    code from the tutor session

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { writeFile } = require('node:fs/promises');
 const generateMarkdown = require('./src/generateMarkdown');
+const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const employeeArray = [];
 
 // array of questions when prompted
 const questions = [
@@ -57,16 +62,6 @@ const questions = [
       }
     },
   },
-  // {
-  //   type: 'checkbox',
-  //   name: 'employee',
-  //   message: "Please choose Engineer, Intern or Exit to finished building your team?",
-  //   choices: [
-  //     "Engineer",
-  //     "Intern",
-  //     "Exit"  
-  //   ],
-  // },
 ]
 
 // function that writes index.html file
@@ -80,11 +75,151 @@ function writeToFile(fileName, data) {
   });
 }
 
+renderEngineer = (engineerData) => {
+  inquire.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the engineer's name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the engineer's ID number? (Required)",
+      validate: (idInput) => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's ID number!");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the engineer's email address? (Required)",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's email address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is the engineer's gitHub name? (Required)",
+      validate: (githubNameInput) => {
+        if (officeNumberInput) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's gitHub name!");
+          return false;
+        }
+      },
+    },
+  ])
+}
+
+renderIntern = (internData) => {
+  inquire.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the intern's name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the intern's name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the intern's ID number? (Required)",
+      validate: (idInput) => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log("Please enter the intern's ID number!");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the intern's email address? (Required)",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter the intern's email address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is the intern's school name? (Required)",
+      validate: (schoolNameInput) => {
+        if (officeNumberInput) {
+          return true;
+        } else {
+          console.log("Please enter the intern's school name!");
+          return false;
+        }
+      },
+    },
+  ])
+}
+
 // function to initialize app
-function init() {
+function init(engineerData) {
   inquirer.prompt(questions)
     .then((answers) => {
-      writeToFile('./dist/index.html', answers);
+      console.log(answers);
+      const mainManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+      employeeArray.push(mainManager);
+      console.log(employeeArray[0].getRole());
+      inquirer.prompt({
+        type: 'checkbox',
+        name: 'confirmAddEmployee',
+        message: "Please choose Engineer, Intern or Exit to finished building your team?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "Exit"
+        ],
+      })
+        .then((data) => {
+          console.log(data)
+          if (data.engineerData === 'Engineer') {
+            renderEngineer(engineerData);
+          }
+        })
+        .then((data) => {
+          console.log(data)
+          if (data.internData === 'Intern') {
+            renderIntern(internData);
+          }
+        })
+      // writeToFile('./dist/index.html', answers);
     })
     .catch((error) => {
       if (error.isTypeError) {
@@ -97,3 +232,16 @@ function init() {
 
 // initialize app
 init();
+
+// choiceData = (choice) => {
+//   let selectedChoice = '';
+//   if ('Intern') {
+//     return selectedChoice.data;
+//   } else if ('Engineer') {
+//     return selectedChoice.data;
+//   } else {
+//     selectedChoice = `Office number: ${data}`;
+//   }
+//   return selectedChoice;
+// }
+
